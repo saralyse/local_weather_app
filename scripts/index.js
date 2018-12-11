e1a803eac36deec472411ef84db0c6021eeeb24a
Sara Simon <sara.a.simon@gmail.com>
2018-12-11 17:29:20 -0500

    install webpack

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
+import * as config from '../config.js';
+
 $(document).ready(() => {
   var fahrenheit, celsius;
   var weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
-  var apiKey = "5784cd5acade608a751f7a6777f5b158";
+  var apiKey = config.OWM_API_KEY;
   getLatLon();
 
   function getLatLon() {
@@ -20,10 +22,11 @@ $(document).ready(() => {
       },
       error: function(error) {
         alert('Oops something went wrong, Please try again.');
-        console.log(err);
+        console.log(error);
       }
     });
   }
+
   function getWeatherData() {
     $.ajax({
       url: weatherUrl,
@@ -31,13 +34,39 @@ $(document).ready(() => {
       dataType: 'json',
       success: function(data) {
         var temperature = data.main.temp;
-        celsius = temperature;
-        fahrenheit = celsius * 1.8 + 32;
-        var icon = data.weather[0].icon;
-        var desc = data.weather[0].main+", "+data.weather[0].description;
+        var celsius = Math.round(temperature);
+        var fahrenheit = Math.round(celsius * 1.8 + 32);
+        var desc = data.weather[0].main + ": " + data.weather[0].description;
+
         $('#desc').html(desc);
-        $('#icon').attr('src','http://openweathermap.org/img/w/'+icon+'.png');
-        $('#temp').html(data.main.temp+" &#8451;")
+        $('#temp').html(data.main.temp + " &#8451;");
+        $('.temp-unit').attr('id', 'c');
+        $('.icon').html("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
+
+
+        /*var date = new Date();
+        var sunrise = new Date(data.sys.sunrise * 1000);
+        var sunset = new Date(data.sys.sunset * 1000);
+
+        if (date.getHours() >= sunrise.getHours() && date.getHours() < sunset.getHours()) {
+          var iconID = 'wi wi-owm-day-${result.data.weather[0].id}';
+        } else if (date.getHours() >= sunset.getHours()) {
+          var iconID = 'wi wi-owm-night-${result.data.weather[0].id}';
+        }
+
+        $('.icon').html("<i class=" + iconID + "></i>");
+        */
+
+
+        $('#toggle-button').on('click', () => {
+            if ($('.temp-unit').attr('id') == 'c') {
+              $('#temp').html(fahrenheit + " &#8457;")
+              $('.temp-unit').attr('id', 'f');
+            } else if ($('.temp-unit').attr('id')=='f') {
+              $('#temp').html(celsius + " &#8451;");
+              $('.temp-unit').attr('id', 'c');
+            }
+        });
       },
       error: function(err) {
         alert('Oops something went wrong, Please try again.');
@@ -45,20 +74,14 @@ $(document).ready(() => {
       }
     });
   }
-  $('#toggle').on('click', () => {
-    if ($('.temp-unit').attr('id')=='c') {
-      $('#temp').html(fahrenheit + " &#8457;");
-      $('.temp-unit').attr('id', 'f');
-    } else if ($('.temp-unit').attr('id')=='f') {
-      $('#temmp').html(celsius+"&#8451;");
-      $('.temp-unit').attr('id', 'c');
-    }
-  });
+  
 
   $('#submit-button').on('click', () => {
     let zip = $('#change-loc').html();
-    $('#submit-button').attr("href", "api.openweathermap.org/data/2.5/weather?zip="+zip+",us");
+    $('#submit-button').attr("href", "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us"+"&APPID="+apiKey+"&units=metric");
   })
+
+
 
 });
 
