6ef10e15817ccd0ae294df9ec92970b19aa1aab8
Sara Simon <sara.a.simon@gmail.com>
2018-12-13 20:01:23 -0500

    added moment and sunset info

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
-
 $(document).ready(() => {
   var fahrenheit, celsius;
   var currentUrl = "https://api.openweathermap.org/data/2.5/weather";
   var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast";
+  var sunUrl = "https://api.sunrise-sunset.org/json";
   var apiKeyC = '5784cd5acade608a751f7a6777f5b158';
-  var apiKeyF = 'c1c8298c3ffde842d6161c2da1ceb5b0'
+  var apiKeyF = 'c1c8298c3ffde842d6161c2da1ceb5b0';
   getLatLon();
 
   function getLatLon() {
@@ -13,9 +13,12 @@ $(document).ready(() => {
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
         currentUrl += "?lat="+lat+"&lon="+lon+"&APPID="+apiKeyC+"&units=metric";
-        forecastUrl += "?lat="+lat+"&lon="+lon+"&APPID="+apiKeyF+"&units=metric";
-        getCurrentData();
-        //getForecastData();
+        forecastUrl += "?lat="+lat+"&lon="+lon+"&APPID="+apiKeyF+"&cnt=4&units=imperial";
+        sunUrl += "?lat="+lat+"&lng="+lon+"&date=today";
+        
+        //getCurrentData();
+        getForecastData();
+        getSunTimes();
       });
     } else if (!navigator.geolocation) {
       $.ajax({
@@ -26,9 +29,11 @@ $(document).ready(() => {
           let lat = data.latitude;
           let lon = data.longitude;
           currentUrl += "?lat="+lat+"&lon="+lon+"&APPID="+apiKeyC+"&units=metric";
-          forecastUrl += "?lat="+lat+"&lon="+lon+"&APPID="+apiKeyF+"&units=metric";
-          getCurrentData();
-          //getForecastData();
+          forecastUrl += "?lat="+lat+"&lon="+lon+"&APPID="+apiKeyF+"&cnt=4&units=imperial";
+          sunUrl += "?lat="+lat+"&lng="+lon+"&date=today";
+          //getCurrentData();
+          getForecastData();
+          getSunTimes();
         },
         error: function(error) {
           
@@ -43,38 +48,134 @@ $(document).ready(() => {
   }
 
 
-  function getCurrentData() {
+  function getSunTimes() {
+    $.ajax({
+      url: sunUrl,
+      type: 'GET',
+      dataType: 'json',
+      sucess: function(data) {
+        var date = new Date();
+        var time = date.getTime();
+        var sunsetStart = data.results.sunset;
+        var sunsetStop = data.results.nautical_twilight_end;
+        var sunriseStart = data.results.nautical_twilight_start;
+        var sunriseStop = data.results.sunrise;
+
+        if (time >= sunsetStart && time <= sunsetStop) {
+          // sunset theme
+          if (code >= 200 && code < 240) {
+            // thunderstorm theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/storm.jpg")',
+              'color': 'white'
+            });
+          } else if (code >= 300 && code <= 531) {
+            // rain theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/rain.jpg")'
+            });
+            $('#city').css({
+              'font-weight': '300'
+            });
+            $('#desc').css({
+              'font-weight': '400'
+            });
+          } else if (code >= 600 && code <=622) {
+             // snow theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/snow.jpg")'
+            });
+          } else if ((code >= 801 && code <= 804) || (code >= 700 && code <= 741)) {
+            // cloudy theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/cloudy.jpg")'
+            });
+          } else {
+            // clear
+            $('.weather-container').css({
+              'background-image': 'url("../img/sunset.jpg")',
+              'color': 'white'
+            });
+          }
+        } else if (time > sunsetStop && time < sunriseStart) {
+          // night theme
+          if (code >= 200 && code < 240) {
+            // thunderstorm theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/storm.jpg")',
+              'color': 'white'
+            });
+          } else if (code >= 300 && code <= 531) {
+            // rain theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/night-rain.jpg")',
+              'color': 'white'
+            });
+          } else if (code >= 600 && code <=622) {
+             // snow theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/snow.jpg")'
+            });
+          } else if ((code >= 801 && code <= 804) || (code >= 700 && code <= 741)) {
+            // cloudy theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/night-cloudy.jpg")',
+              'color': 'white'
+            });
+          } else {
+            // night clear
+            $('.weather-container').css({
+              'background-image': 'url("../img/night.jpg")',
+              'color': 'white'
+            });
+          }
+        } else {
+          if (code >= 200 && code < 240) {
+            // thunderstorm theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/storm.jpg")',
+              'color': 'white'
+            });
+          } else if (code >= 300 && code <= 531) {
+            // rain theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/rain.jpg")'
+            });
+            $('#city').css({
+              'font-weight': '300'
+            });
+            $('#desc').css({
+              'font-weight': '400'
+            });
+          } else if (code >= 600 && code <=622) {
+             // snow theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/snow.jpg")'
+            });
+          } else if ((code >= 801 && code <= 804) || (code >= 700 && code <= 741)) {
+            // cloudy theme
+            $('.weather-container').css({
+              'background-image': 'url("../img/cloudy.jpg")'
+            });
+          } else {
+            $('.weather-container').css({
+              'background-image': 'url("../img/day.jpg")'
+            });
+          }
+        };
+
+      }
+    })
+
+  }
+
+  /*function getCurrentData() {
     $.ajax({
       url: currentUrl,
       type: 'GET',
       dataType: 'json',
       success: function(data) {
-        let temperature = data.main.temp;
-        var celsius = Math.round(temperature);
-        var fahrenheit = Math.round(celsius * 1.8 + 32);
-        var desc = data.weather[0].main;
-        var code = [data.weather[0].id];
-        var unitC = " &#8451;";
-        var unitF = " &#8457;";
-        //var code = 301;
-        $('#city').html(data.name);
-
-        $('#desc').html(data.weather[0].main);
-        $('#temp').html(fahrenheit);
-        $('.temp-unit').attr('id', 'f').html(unitF);
-        $('.icon').html("<i class='wi wi-owm-" + data.weather[0].id + "'></i>");
-
-        $('#toggle-button').on('click', () => {
-            if ($('.temp-unit').attr('id') == 'c') {
-              $('#temp').html(fahrenheit);
-              $('.temp-unit').attr('id', 'f').html(unitF);
-            } else if ($('.temp-unit').attr('id')=='f') {
-              $('#temp').html(celsius);
-              $('.temp-unit').attr('id', 'c').html(unitC);
-            }
-        });
-
-
+        
         function setPhoto () {
           var date = new Date();
           var currentTime = date.getHours();
@@ -187,41 +288,65 @@ $(document).ready(() => {
               });
             }
           };
-        };
+        }
         setPhoto();
-      },
+        },
       error: function(err) {
         alert('Oops something went wrong, Please try again.');
         console.log(err);
       }
     });
   }
+  */
+  
+  
 
-
-  /*function getForecastData() {
+  function getForecastData() {
     $.ajax({
       url: forecastUrl,
       type: 'GET',
       dataType: 'json',
       success: function(data) {
-        function getTemp() {
-          for (i = 0; i < 5; i++) {
-            var fcTemp = data.list[i].main.temp;
-          }
+        var temp;
+        var fahrenheit = Math.round(temp);
+        var celsius = Math.round((fahrenheit -32) / 1.8);
+        var unitC = '<span class="unit"> &#8451;</span>';
+        var unitF = '<span class="unit"> &#8457;</span>';
+
+        $('#city').html(data.city.name);
+
+        for (i = 0; i < data.list.length; i++) {
+          let temp = (data.list[i].main.temp);
+          let fahrenheit = Math.round(temp);
+          let celsius = Math.round((fahrenheit - 32) / 1.8);
+
+          $('#f' + i).html(fahrenheit + unitF);
+          $('#c' + i).html(celsius + unitC);
+
+          $('.toggle-c').hide();
+
+          $('#toggle-button').on('click', () => {
+            $('.toggle-f, .toggle-c').toggle();
+          });
+              
+    
+          var icon = data.list[i].weather[0].id;
+          $('#icon' + i).html("<i class='wi wi-owm-" + icon + "'></i>");
+              
+          var desc = data.list[i].weather[0].main;
+          $('#desc' + i).html(desc);
+
+          var dayName = moment().add(i, "day").format("ddd");
+          $('#day' + i).html(dayName);
         }
-        //var fcTemp = data.list.main.temp;
-        var fcC = Math.round(fcTemp);
-        var fcF = Math.round(fcC * 1.8 + 32);
-        
-        $('#fc-temp-1').html(fcF + " &#8457;");
-        $('#fc-temp-2').html(fcF + " &#8457;");
+
       },
       error: function(err) {
         alert('Oops something went wrong, Please try again.');
-        console.log(err);
+         console.log(err);
       }
     });
-  }*/
+  }
 
 
 });
